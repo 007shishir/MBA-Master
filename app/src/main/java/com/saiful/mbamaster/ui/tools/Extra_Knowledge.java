36321cb6eba3cb67041f298b963d2aa3bad39b6b
007shishir <shishir177753@gmail.com>
2020-01-22 13:56:53 +0600

    correction in memorise section

--- app/src/main/java/com/saiful/mbamaster/ui/tools/Extra_Knowledge.java
+++ app/src/main/java/com/saiful/mbamaster/ui/tools/Extra_Knowledge.java
@@ -19,20 +19,10 @@ import com.saiful.mbamaster.R;
 public class Extra_Knowledge extends Fragment implements View.OnClickListener {
 
     private ExtraKnowledgeViewModel ekViewModel;
-<<<<<<< HEAD
 
-    private LinearLayout mLL_mbi, mLL_networkingFCM, mLL_ProjectMngt,
+    private LinearLayout mLL_smis, mLL_networkingFCM, mLL_ProjectMngt,
             mLL_humanRIS;
 
-=======
-<<<<<<< HEAD
-    private LinearLayout mLL_mbi, mLL_networkingFCM, mLL_ProjectMngt,
-            mLL_humanRIS;
-=======
-    private LinearLayout mLL_mbi, mLL_awsQuickSight, mLL_seo, mLL_digitalMarketing, mLL_cSharp,
-            mLL_unity;
->>>>>>> 3b854cff864f9575a477f436abfc13417d6291ef
->>>>>>> origin/master
     private Bundle topic;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -41,39 +31,16 @@ public class Extra_Knowledge extends Fragment implements View.OnClickListener {
                 ViewModelProviders.of(this).get(ExtraKnowledgeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_extra_knowledge, container, false);
 //        final TextView textView = root.findViewById(R.id.text_tools);
-        mLL_mbi = root.findViewById(R.id.mLL_mbi);
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> origin/master
+        mLL_smis = root.findViewById(R.id.mLL_smis);
         mLL_networkingFCM = root.findViewById(R.id.mLL_networkingFCM);
         mLL_ProjectMngt = root.findViewById(R.id.mLL_ProjectMngt);
         mLL_humanRIS = root.findViewById(R.id.mLL_humanRIS);
 
 
-        mLL_mbi.setOnClickListener(this);
+        mLL_smis.setOnClickListener(this);
         mLL_networkingFCM.setOnClickListener(this);
         mLL_ProjectMngt.setOnClickListener(this);
         mLL_humanRIS.setOnClickListener(this);
-<<<<<<< HEAD
-=======
-=======
-        mLL_awsQuickSight = root.findViewById(R.id.mLL_awsQuickSight);
-        mLL_seo = root.findViewById(R.id.mLL_seo);
-        mLL_digitalMarketing = root.findViewById(R.id.mLL_digitalMarketing);
-        mLL_cSharp = root.findViewById(R.id.mLL_cSharp);
-        mLL_unity = root.findViewById(R.id.mLL_unity);
-
-
-        mLL_mbi.setOnClickListener(this);
-        mLL_awsQuickSight.setOnClickListener(this);
-        mLL_seo.setOnClickListener(this);
-        mLL_digitalMarketing.setOnClickListener(this);
-        mLL_cSharp.setOnClickListener(this);
-        mLL_unity.setOnClickListener(this);
->>>>>>> 3b854cff864f9575a477f436abfc13417d6291ef
->>>>>>> origin/master
-
 
         ekViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -87,55 +54,15 @@ public class Extra_Knowledge extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.mLL_mbi:
+            case R.id.mLL_smis:
                 topic = new Bundle();
                 topic.putString("topic_name", "Strategic Management &amp; Information Systems");
-<<<<<<< HEAD
                 topic.putString("topic_code", "smis");
-=======
-<<<<<<< HEAD
-                topic.putString("topic_code", "smis");
-=======
-                topic.putString("topic_code", "mbi");
-                Navigation.findNavController(v).
-                        navigate(R.id.action_nav_extra_knowledge_to_ek_mcq_memorise_option, topic);
-                break;
-            case R.id.mLL_awsQuickSight:
-                topic = new Bundle();
-                topic.putString("topic_name", "Information Securities Management Systems");
-                topic.putString("topic_code", "aqs");
-                Navigation.findNavController(v).
-                        navigate(R.id.action_nav_extra_knowledge_to_ek_mcq_memorise_option, topic);
-                break;
-            case R.id.mLL_seo:
-                topic = new Bundle();
-                topic.putString("topic_name", "Management Control System");
-                topic.putString("topic_code", "seo");
-                Navigation.findNavController(v).
-                        navigate(R.id.action_nav_extra_knowledge_to_ek_mcq_memorise_option, topic);
-                break;
-            case R.id.mLL_digitalMarketing:
-                topic = new Bundle();
-                topic.putString("topic_name", "Knowledge Management");
-                topic.putString("topic_code", "dmk");
-                Navigation.findNavController(v).
-                        navigate(R.id.action_nav_extra_knowledge_to_ek_mcq_memorise_option, topic);
-                break;
-            case R.id.mLL_cSharp:
-                topic = new Bundle();
-                topic.putString("topic_name", "Total Quality Management");
-                topic.putString("topic_code", "csh");
->>>>>>> 3b854cff864f9575a477f436abfc13417d6291ef
->>>>>>> origin/master
                 Navigation.findNavController(v).
                         navigate(R.id.action_nav_extra_knowledge_to_ek_mcq_memorise_option, topic);
                 break;
             case R.id.mLL_networkingFCM:
                 topic = new Bundle();
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> origin/master
                 topic.putString("topic_name", "Networking For Corporate Management");
                 topic.putString("topic_code", "nfcms");
                 Navigation.findNavController(v).
@@ -155,16 +82,6 @@ public class Extra_Knowledge extends Fragment implements View.OnClickListener {
                 Navigation.findNavController(v).
                         navigate(R.id.action_nav_extra_knowledge_to_ek_mcq_memorise_option, topic);
                 break;
-<<<<<<< HEAD
-=======
-=======
-                topic.putString("topic_name", "Human Resource Information System");
-                topic.putString("topic_code", "uty");
-                Navigation.findNavController(v).
-                        navigate(R.id.action_nav_extra_knowledge_to_ek_mcq_memorise_option, topic);
-                break;
->>>>>>> 3b854cff864f9575a477f436abfc13417d6291ef
->>>>>>> origin/master
             default:
                 Toast.makeText(getContext(), "Choose a Option", Toast.LENGTH_SHORT).show();
                 break;
